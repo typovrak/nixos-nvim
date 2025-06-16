c6c3a3110212e38cfc85cb2dc0206efe1a4f89a0
typovrak <typovrak@gmail.com>
2025-06-16 23:30:00 +0200

    remove hardtime

--- configuration.nix
+++ configuration.nix
@@ -71,10 +71,6 @@ in {
 		chown ${username}:${group} ${home}/.config/nvim/lua/plugins/guess-indent.lua
 		chmod 600 ${home}/.config/nvim/lua/plugins/guess-indent.lua
 
-		cp ${./hardtime.lua} ${home}/.config/nvim/lua/plugins/hardtime.lua
-		chown ${username}:${group} ${home}/.config/nvim/lua/plugins/hardtime.lua
-		chmod 600 ${home}/.config/nvim/lua/plugins/hardtime.lua
-
 		cp ${./oil.lua} ${home}/.config/nvim/lua/plugins/oil.lua
 		chown ${username}:${group} ${home}/.config/nvim/lua/plugins/oil.lua
 		chmod 600 ${home}/.config/nvim/lua/plugins/oil.lua
